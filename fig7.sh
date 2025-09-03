51b5d92f58fd9418672f393dd72274ab8461601a
volatill <510112342@qq.com>
2025-09-03 20:49:38 +0800

    reproduce: update figure 7 scripts

--- fig7.sh
+++ fig7.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-datasets=('com-dblp.ungraph.json3')
+DATASET_ALIAS="${1:-null}"
+resolve_dataset() {
+  case "$1" in
+    dblp)       echo "com-dblp.ungraph.json3" ;;
+    wikipedia)
+                echo "wikipedia.json3" ;;
+    orkut)      echo "com-orkut.ungraph.json3" ;;
+    twitter)
+                echo "twitter-2010.json3" ;;
+    *)
+      echo "Unknown dataset alias: $1" >&2
+      return 1
+      ;;
+  esac
+}
+dataset="$(resolve_dataset "$DATASET_ALIAS")"
+datasets=($dataset)
 vnum=3072627
 algms=('basics.groovy')
 
--- fig7_property.sh
+++ fig7_property.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-# python3 scripts/process_property_dataset.py
-# python3 scripts/process_property_freebase.py
-
+mkdir dataset
+python3 scripts/process_property_ldbc.py
+python3 scripts/process_property_freebase.py
 # datasets=(ldbc.json2 freebase_large.json2)
-datasets=(ldbc.json2)
+
+DATASET_ALIAS="${1:-null}"
+resolve_dataset() {
+  case "$1" in
+    ldbc)       echo "ldbc.json2"      
+    ;;
+    freebase)
+                echo "freebase_large.json2" 
+                ;;
+    *)
+      echo "Unknown dataset alias: $1" >&2
+      return 1
+      ;;
+  esac
+}
+dataset="$(resolve_dataset "$DATASET_ALIAS")"
+datasets=($dataset)
 
 export LD_PRELOAD="$(
   ( /sbin/ldconfig -p 2>/dev/null || /usr/sbin/ldconfig -p 2>/dev/null || ldconfig -p ) \
@@ -12,8 +28,9 @@ export LD_PRELOAD="$(
 
 for ds in "${datasets[@]}"
 do
+  echo $ds
   rm -rf /tmp/demo
-  path=$HOME/AsterDB/dataset/$ds
+  path=$(pwd)/dataset/$ds
   cp scripts/load_with_properties.groovy ./tmp.groovy
   sed -i "s|dataset = PATH_TO_DATASET|dataset='$path'|g" tmp.groovy
   bin/gremlin.sh -e tmp.groovy
